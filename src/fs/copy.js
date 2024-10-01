29b526a4f8c186582626fa58da84059d6e51fbb3
Izy <126877709+izy-code@users.noreply.github.com>
2024-10-01 23:32:04 +0300

    feat: add implementation of copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,35 @@
+import { join } from 'path';
+import { cp } from 'fs/promises';
+import { getModuleDirName, isPathAccessible } from '../utils.js';
+
+const SOURCE_DIR_NAME = 'files';
+const COPY_DIR_NAME = 'files_copy';
+const ERROR_MESSAGE = 'FS operation failed';
+
+const __dirname = getModuleDirName(import.meta.url);
+
 const copy = async () => {
-    // Write your code here 
+    const sourceDirPath = join(__dirname, SOURCE_DIR_NAME);
+    const copyDirPath = join(__dirname, COPY_DIR_NAME);
+    const isCopyDirExists = await isPathAccessible(copyDirPath);
+
+    if (isCopyDirExists) {
+        throw new Error(ERROR_MESSAGE);
+    }
+
+    try {
+        await cp(sourceDirPath, copyDirPath, {
+            recursive: true,
+            force: false,
+            errorOnExist: true,
+        });
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            throw new Error(ERROR_MESSAGE);
+        } else {
+            throw new Error(err);
+        }
+    }
 };
 
 await copy();
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,16 @@
-import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { access } from 'fs/promises';
 
 export const getModuleFileName = (moduleUrl) => fileURLToPath(moduleUrl);
 export const getModuleDirName = (moduleUrl) => dirname(getModuleFileName(moduleUrl));
+
+export const isPathAccessible = async path => {
+    try {
+        await access(path);
+
+        return true;
+    } catch {
+        return false;
+    }
+};
