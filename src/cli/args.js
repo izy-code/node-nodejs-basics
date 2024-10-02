35291c5732806a26d0b405fa3cd81e32c77e33da
Izy <126877709+izy-code@users.noreply.github.com>
2024-10-02 14:57:24 +0300

    feat: add implementation of args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
+const ADDITIONAL_CLI_ARGS_START = 2
+const INDEX_OFFSET = 2;
+const PREFIX_LENGTH = 2;
+
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(ADDITIONAL_CLI_ARGS_START);
+    const formattedArgPairs = [];
+
+    for (let i = 0; i < args.length; i += INDEX_OFFSET) {
+        formattedArgPairs.push(`${args[i].slice(PREFIX_LENGTH)} is ${args[i + 1]}`);
+    }
+
+    console.log(formattedArgPairs.join(', '));
 };
 
 parseArgs();
