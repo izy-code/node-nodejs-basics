eb59d2549b0f4a2f027b22e1dfec32d2fb267231
Izy <126877709+izy-code@users.noreply.github.com>
2024-10-02 13:39:23 +0300

    feat: add implementation of env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,19 @@
+import { env } from 'process';
+
+const PREFIX = 'RSS_';
+
+const noPrefixedVarsMessage = `No environment variables with prefix "${PREFIX}" found`;
+
 const parseEnv = () => {
-    // Write your code here 
+    const prefixedEnvEntries = Object.entries(env).filter(([key]) => key.startsWith(PREFIX));
+
+    if (prefixedEnvEntries.length) {
+        const result = prefixedEnvEntries.map(([key, value]) => `${key}=${value}`).join('; ');
+
+        console.log(result);
+    } else {
+        console.log(noPrefixedVarsMessage);
+    }
 };
 
 parseEnv();
