b4bc6b141e7297f043062d63d6f7d35975a04721
Izy <126877709+izy-code@users.noreply.github.com>
2024-10-02 13:42:45 +0300

    fix: del import from env function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,11 +1,9 @@
-import { env } from 'process';
-
 const PREFIX = 'RSS_';
 
 const noPrefixedVarsMessage = `No environment variables with prefix "${PREFIX}" found`;
 
 const parseEnv = () => {
-    const prefixedEnvEntries = Object.entries(env).filter(([key]) => key.startsWith(PREFIX));
+    const prefixedEnvEntries = Object.entries(process.env).filter(([key]) => key.startsWith(PREFIX));
 
     if (prefixedEnvEntries.length) {
         const result = prefixedEnvEntries.map(([key, value]) => `${key}=${value}`).join('; ');
