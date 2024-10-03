8eba068725f20c17e10ba6109b463243d0e14e21
Izy <126877709+izy-code@users.noreply.github.com>
2024-10-03 19:42:25 +0300

    feat: add implementation of decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,25 @@
+import { join } from 'path';
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+import { getModuleDirName } from '../utils.js';
+
+const DIR_NAME = 'files';
+const INPUT_FILE_NAME = 'archive.gz';
+const OUTPUT_FILE_NAME = 'fileToCompress.txt';
+
+const __dirname = getModuleDirName(import.meta.url);
+
 const decompress = async () => {
-    // Write your code here 
+    const inputFilePath = join(__dirname, DIR_NAME, INPUT_FILE_NAME);
+    const outputFilePath = join(__dirname, DIR_NAME, OUTPUT_FILE_NAME);
+
+    const readStream = createReadStream(inputFilePath);
+    const gunzipStream = createGunzip();
+    const writeStream = createWriteStream(outputFilePath);
+
+    readStream
+        .pipe(gunzipStream)
+        .pipe(writeStream);
 };
 
 await decompress();
