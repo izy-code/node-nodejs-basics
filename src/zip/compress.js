da7c8cc8b641ce068ab4e1ac2eeeb6d30efa6be1
Izy <126877709+izy-code@users.noreply.github.com>
2024-10-03 18:53:24 +0300

    feat: add implementation of compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import { join } from 'path';
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+import { getModuleDirName } from '../utils.js';
+
+const DIR_NAME = 'files';
+const INPUT_FILE_NAME = 'fileToCompress.txt';
+const OUTPUT_FILE_NAME = 'archive.gz';
+
+const __dirname = getModuleDirName(import.meta.url);
+
 const compress = async () => {
-    // Write your code here 
+    const inputFilePath = join(__dirname, DIR_NAME, INPUT_FILE_NAME);
+    const outputFilePath = join(__dirname, DIR_NAME, OUTPUT_FILE_NAME);
+
+    const readStream = createReadStream(inputFilePath);
+    const gzipStream = createGzip();
+    const writeStream = createWriteStream(outputFilePath);
+
+    readStream
+        .pipe(gzipStream)
+        .pipe(writeStream);
 };
 
 await compress();
