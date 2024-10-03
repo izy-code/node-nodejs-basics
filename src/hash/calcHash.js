1364cfb54e6af4511d0f43abd1be65d3ba1ed7a5
Izy <126877709+izy-code@users.noreply.github.com>
2024-10-03 13:41:48 +0300

    feat: add implementation of calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import { pipeline } from 'stream/promises';
+import { getModuleDirName } from '../utils.js';
+
+const DIR_NAME = 'files';
+const FILE_NAME = 'fileToCalculateHashFor.txt';
+const ALGORITHM = 'sha256';
+const ENCODING = 'hex';
+
+const __dirname = getModuleDirName(import.meta.url);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, DIR_NAME, FILE_NAME);
+    const inputStream = createReadStream(filePath);
+    const hashStream = createHash(ALGORITHM);    
+    
+    await pipeline(inputStream, hashStream.setEncoding(ENCODING), process.stdout, { end: false });
+    console.log('\n');
 };
 
 await calculateHash();
