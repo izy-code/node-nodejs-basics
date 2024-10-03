e6c6fe9c0b3143456bd332701ae839621cd89e99
Izy <126877709+izy-code@users.noreply.github.com>
2024-10-03 16:51:36 +0300

    feat: add implementation of write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,17 @@
+import { join } from 'path';
+import { createWriteStream } from 'fs';
+import { getModuleDirName } from '../utils.js';
+
+const DIR_NAME = 'files';
+const FILE_NAME = 'fileToWrite.txt';
+
+const __dirname = getModuleDirName(import.meta.url);
+
 const write = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, DIR_NAME, FILE_NAME);
+    const outputStream = createWriteStream(filePath, 'utf-8');
+
+    process.stdin.pipe(outputStream);
 };
 
 await write();
