ca13c2809f09cb3094215ed4d6348bdf2e13bd9b
Izy <126877709+izy-code@users.noreply.github.com>
2024-10-03 16:30:50 +0300

    feat: add implementation of read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,20 @@
+import { join } from 'path';
+import { createReadStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import { EOL } from 'os';
+import { getModuleDirName } from '../utils.js';
+
+const DIR_NAME = 'files';
+const FILE_NAME = 'fileToRead.txt';
+
+const __dirname = getModuleDirName(import.meta.url);
+
 const read = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, DIR_NAME, FILE_NAME);
+    const inputStream = createReadStream(filePath, 'utf-8');
+
+    await pipeline(inputStream, process.stdout, { end: false });
+    console.log(EOL);
 };
 
 await read();
